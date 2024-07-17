7953aea063169b3ca354606f7de76fba02d8e9f5
Goulven Champenois <goulven.champenois@beta.gouv.fr>
2024-07-17 14:44:34 +0200

    Squashed commit of the following:
    commit 4603a931a3f612905af8bf98577af2b3b9311b68
    Author: Goulven Champenois <goulven.champenois@beta.gouv.fr>
    Date:   Wed Jul 17 12:07:26 2024 +0200
    
        Corrige la requête des recensements d'objets déplacés
    
    commit 26196d7a62255172646c3336d8ebc690a0b338fc
    Author: Goulven Champenois <goulven.champenois@beta.gouv.fr>
    Date:   Wed Jul 17 11:50:02 2024 +0200
    
        Supprime le texte "loca" qui était ajouté en fin de certaines notes
    
    commit 053592ce4776af7983540251fafc4419d60bca9e
    Author: Goulven Champenois <goulven.champenois@beta.gouv.fr>
    Date:   Wed Jul 17 11:09:44 2024 +0200
    
        Réorganise les colonnes de l'export des objets manquants
    
    commit 67ef72ba45f177118efda118022f5ce4337abf4a
    Author: Goulven Champenois <goulven.champenois@beta.gouv.fr>
    Date:   Wed Jul 17 10:42:56 2024 +0200
    
        Supprime une N+1 dans l'export Pop
    
    commit c8606eb7e26f085488e9263d81d7f6ed4fdc1ba4
    Author: Goulven Champenois <goulven.champenois@beta.gouv.fr>
    Date:   Wed Jul 17 10:35:12 2024 +0200
    
        Supprime l'ancien export Palissy
    
    commit 6aa71b2ddea272d80cb3faf407700b8a255eccc6
    Author: Goulven Champenois <goulven.champenois@beta.gouv.fr>
    Date:   Wed Jul 17 10:33:20 2024 +0200
    
        Ajustement des intitulés de liens pour l'accessibilité
    
    commit 3589b7bb9c3e181c1b2b8a928d890925cb56617e
    Author: Goulven Champenois <goulven.champenois@beta.gouv.fr>
    Date:   Wed Jul 17 10:20:37 2024 +0200
    
        Renomme les liens d'export dans la page d'admin

--- app/models/exports/mpp.rb
+++ app/models/exports/mpp.rb
@@ -107,6 +107,7 @@ module Exports
           objet.palissy_REF,
           objet.departement.nom,
           objet.lieu_actuel_code_insee,
+          objet.recensement.notes,
           objet.recensement.dossier.notes_commune,
           objet.recensement.dossier.notes_conservateur,
           objet.recensement.notes,
